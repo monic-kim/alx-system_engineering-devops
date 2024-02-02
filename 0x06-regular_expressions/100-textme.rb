22f7c16ffd75390436df5318799712aeee970add
monic-kim <wambuimonica506@gmail.com>
2024-02-02 01:11:55 -0800

    regular expressions

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+#SENDER = ARGV[0].scan(/from:\+*\w*/).join[5..-1]
+#SENDER = ARGV[0].scan(/to:\+*\w*/).join[3..-1]
+#SENDER = ARGV[0].scan(/flags:(.*?)\I/).join
+#
+#msg = SENDER + "," + RECEIVER + "," + FLAGS
+#puts msg
